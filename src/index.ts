dd2ebc2ce5a46192130013a1d55bf892104c4267
MrDaPoyo <wiiuforever25@gmail.com>
2025-01-25 15:35:42 +0100

    hello world

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello World").listen(3000);
+const app = new Elysia();
+
+app.get("/", () => {
+  return "Hello, World!";
+});
+
+app.listen(3000);
 
 console.log(
   `The PoyoWeb is running at ${app.server?.hostname}:${app.server?.port}`
