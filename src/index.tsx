d3440d7b6900d8f4af522538bb9fd5f629bf3d16
MrDaPoyo <wiiuforever25@gmail.com>
2025-01-26 17:25:18 +0100

    auth middleware that idk how to use

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import { Elysia } from 'elysia';
+import { jwt } from '@elysiajs/jwt';
 import { html, Html } from '@elysiajs/html';
 
 import { db, setupDB } from './db';
@@ -14,6 +15,28 @@ const app = new Elysia();
 
 app.use(html());
 app.use(auth.setup);
+app.use(jwt({
+  name: 'jwt',
+  secret: process.env.AUTH_SECRET || 'superdupersecretthatssuperdupersecret1234',
+}));
+
+const authMiddleware = async (jwt: any, context: any, next: any) => {
+  const cookie = context.request.headers.cookie;
+  const token = cookie?.auth;
+  if (token) {
+    try {
+      // Verify the token (replace 'your-secret-key' with your actual key)
+      const decoded = jwt.verify(token);
+      context.user = decoded; // Attach user data to the context
+      return true; // Token is valid
+    } catch (err) {
+      return false; // Invalid or expired token
+    }
+  }
+
+  context.user = false; // Mark user as not logged in
+  return false; // No token provided
+};
 
 app.get('/', () => {
   return (
