e172680e4c66874d922f99b97c0546e041ce7ed6
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2025-01-31 11:27:40 +0000

    fix

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,5 @@
 import { Html, html } from '@elysiajs/html';
+import React from 'react';
 
 interface User {
     username: string;
@@ -11,21 +12,23 @@ interface Props {
     user?: User;
 }
 
-const indexHtml = ({ user }: Props) => ( 
-    <>
-        <h1>Welcome to the PoyoWeb!</h1>
-        <p>This is the PoyoWeb's dev server.</p>
-        {user ? (
-            <div>
-                <p>Welcome, {user.username}!</p>
-                <span>Access the <a href="/dashboard">Dashboard</a> - <a href="/auth/logout">Logout</a></span>
-            </div>
-        ) : (
-            <p>
-                <a href="/auth">Authenticate</a> yourself, <strong>guest</strong>.
-            </p>
-        )}
-    </>
-);
+export function indexHtml({ user }: React.PropsWithoutRef<Props>) {
+    return (
+        <>
+            <h1>Welcome to the PoyoWeb!</h1>
+            <p>This is the PoyoWeb's dev server.</p>
+            {user ? (
+                <div>
+                    <p>Welcome, {user.username}!</p>
+                    <span>Access the <a href="/dashboard">Dashboard</a> - <a href="/auth/logout">Logout</a></span>
+                </div>
+            ) : (
+                <p>
+                    <a href="/auth">Authenticate</a> yourself, <strong>guest</strong>.
+                </p>
+            )}
+        </>
+    );
+}
 
 export default indexHtml;
