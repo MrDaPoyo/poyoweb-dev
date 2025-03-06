c6dc8127ca33be9cd8895c6aca18e7cdabc64087
MrDaPoyo <wiiuforever25@gmail.com>
2025-03-06 21:18:21 +0100

    fixed the bun adapter and relocated the postgres docker image

--- poyoweb-client/src/routes/+page.svelte
+++ poyoweb-client/src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Welcome to The PoyoWeb!</h1>
+<p>This is the beggining of a great webhost (hopefully hehe :P frfr)</p>
