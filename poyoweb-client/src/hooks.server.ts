877744a76c3c1d8e51ddd3801d5b8d32aceb217a
MrDaPoyo <wiiuforever25@gmail.com>
2025-05-04 22:17:07 +0200

    restart

--- poyoweb-client/src/hooks.server.ts
+++
@@ -1,8 +0,0 @@
-import type { Handle } from '@sveltejs/kit';
-import { requireAuth } from '$lib/auth';
-
-export const handle: Handle = async ({ event, resolve }) => {
-    const user = await requireAuth(event.cookies);
-    event.locals.user = user;
-    return resolve(event);
-};
